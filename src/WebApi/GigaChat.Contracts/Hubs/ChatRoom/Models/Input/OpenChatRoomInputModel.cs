6a2030e597a02dbc9ac5cf7c88a4b2e5b4edf5b2
Nanoster1 <danil2002.11@mail.ru>
2023-06-14 04:55:16 +0500

    Fixed again

--- src/WebApi/GigaChat.Contracts/Hubs/ChatRoom/Models/Input/OpenChatRoomInputModel.cs
+++ src/WebApi/GigaChat.Contracts/Hubs/ChatRoom/Models/Input/OpenChatRoomInputModel.cs
@@ -1,3 +1,3 @@
 namespace GigaChat.Contracts.Hubs.ChatRoom.Models.Input;
 
-public record OpenChatRoomInputModel(string Title, IEnumerable<Guid> UserIds);
+public record OpenChatRoomInputModel(string Title, ICollection<Guid> UserIds);
--- src/WebApi/GigaChat.Server/Controllers/ErrorController.cs
+++ src/WebApi/GigaChat.Server/Controllers/ErrorController.cs
@@ -1,5 +1,9 @@
 using GigaChat.Contracts.Common.Routes;
+using GigaChat.Contracts.Hubs.ChatRoom.Models.Input;
+using GigaChat.Core.ChatRooms.Commands.OpenChatRoom;
 using GigaChat.Server.Controllers.Common;
+
+using MapsterMapper;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +13,25 @@ namespace GigaChat.Server.Controllers;
 [Route(ServerRoutes.Controllers.ErrorController)]
 public class ErrorController : ApiController
 {
+    private readonly IMapper _mapper;
+
+    public ErrorController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
     [HttpGet]
     [AllowAnonymous]
     public ActionResult Error()
     {
         return Problem();
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public ActionResult Test(OpenChatRoomInputModel inputModel)
+    {
+        var request = _mapper.Map<OpenChatRoomCommand>((Guid.NewGuid(), inputModel));
+        return Ok();
     }
 }
