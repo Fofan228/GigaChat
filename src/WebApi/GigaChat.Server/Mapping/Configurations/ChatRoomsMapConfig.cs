fdcd385e8d15cb51d38f604aa6a285d6cf02ec66
Fofan <ivan.efimov3228@gmail.com>
2023-06-13 22:17:00 +0500

    Added mapping chat room

--- src/WebApi/GigaChat.Server/Mapping/Configurations/ChatRoomsMapConfig.cs
+++ src/WebApi/GigaChat.Server/Mapping/Configurations/ChatRoomsMapConfig.cs
@@ -1,9 +1,5 @@
-using GigaChat.Contracts.Http.ChatRooms.Requests;
-using GigaChat.Contracts.Http.ChatRooms.Responses;
+using GigaChat.Contracts.Hubs.Chat.Models.Input;
 using GigaChat.Core.ChatRooms.Commands.OpenChatRoom;
-using GigaChat.Core.ChatRooms.Commands.CloseChatRoom;
-using GigaChat.Core.ChatRooms.Commands.UpdateChatRoomTitle;
-using GigaChat.Core.Common.Entities.ChatRooms;
 
 using Mapster;
 
@@ -13,9 +9,6 @@ public class ChatRoomsMapConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<ChatRoom, ChatRoomResponse>();
-        config.NewConfig<CreateChatRoomRequest, OpenChatRoomCommand>();
-        config.NewConfig<UpdateChatRoomTitleRequest, UpdateChatRoomTitleCommand>();
-        config.NewConfig<SoftDeleteChatRoomRequest, CloseChatRoomCommand>();
+        config.NewConfig<(Guid id, OpenChatRoomInputModel), OpenChatRoomCommand>().Map(m => m.OwnerId, s => s.id);
     }
 }
